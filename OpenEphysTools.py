cb8851012165b13bc1ef2a2dfc3c57916f3c49da
Yifang-Yuan <77569999+Yifang-Yuan@users.noreply.github.com>
2024-02-16 11:10:46 +0000

    Update OpenEphysTools.py

--- OpenEphysTools.py
+++ OpenEphysTools.py
@@ -6,7 +6,6 @@ PACKAGE THAT NEED FOR THIS ANALYSIS
 https://github.com/open-ephys/open-ephys-python-tools
 https://github.com/pynapple-org/pynapple
 https://github.com/PeyracheLab/pynacollada#getting-started
-https://github.com/PeyracheLab/pynacollada/blob/main/pynacollada/PETH/Tutorial_PETH_Ripples.ipynb
 
 These are functions that I will call in main analysis.
 """
